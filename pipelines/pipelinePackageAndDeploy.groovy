75f28177b62afe54dff1fc8345a43346e41ba3f8
subha <subhasri.dasari@bestandless.com.au>
2026-07-07 14:22:28 +1000

    commit for HS-JDK21-Composable-Storefront

--- pipelines/pipelinePackageAndDeploy.groovy
+++ pipelines/pipelinePackageAndDeploy.groovy
@@ -32,22 +32,22 @@ pipeline {
             }
         }
 
-/*        stage('Deploy') {
+       stage('Deploy') {
             steps{
                 script{
                     deploymentCode = commerceCloudDeploy(codeNumber, "${params.DB_UPDATE_MODE}", "${params.ENVIRONMENT_ID}", "${params.DEPLOY_STRATEGY}")
                     commerceCloudDeployCheck(deploymentCode)
                 }
             }
-        }*/
-        stage('Deploy') {
+        }
+/*        stage('Deploy') {
             when {
                 expression { false }
             }
             steps {
                 echo "Deployment disabled"
             }
-        }
+        }*/
     }
 
     // post build actions
